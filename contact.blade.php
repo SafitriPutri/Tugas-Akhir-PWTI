ce2a1174b61ab41575b21cb98055657553172fdd
SafitriPutri <117289241+SafitriPutri@users.noreply.github.com>
2024-06-02 18:57:40 +0700

    Update contact.blade.php

--- contact.blade.php
+++ contact.blade.php
@@ -1,11 +1,9 @@
-<!DOCTYPE html>
-<html>
-<head>
-    <title>Contact - Safitri Rahayu Kurnia Putri</title>
-</head>
-<body>
-    <h1>Contact Me</h1>
-    <p>Email: safitriputri@student.ub.ac.id</p>
-    <p>Phone: 0822-4574-7512</p>
-</body>
-</html>
+@extends('layouts.main')
+
+@section('title', 'Contact - Safitri Rahayu Kurnia Putri')
+
+@section('content')
+<h1>Contact Me</h1>
+<p>Email: safitriputri@student.ub.ac.id</p>
+<p>Phone: 0822-4574-7512</p>
+@endsection
