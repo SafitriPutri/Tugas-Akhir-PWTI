1c2d7d10047cfc5e260145ce0efb408cf6a311a4
SafitriPutri <117289241+SafitriPutri@users.noreply.github.com>
2024-06-02 18:35:38 +0700

    Add files via upload

---
+++ contact.blade.php
@@ -0,0 +1,11 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Contact - Safitri Rahayu Kurnia Putri</title>
+</head>
+<body>
+    <h1>Contact Me</h1>
+    <p>Email: safitriputri@student.ub.ac.id</p>
+    <p>Phone: 0822-4574-7512</p>
+</body>
+</html>
