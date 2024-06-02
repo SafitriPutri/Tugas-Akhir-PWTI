80c5f4dc1a66157e5aaf1bcbc38f0fdf59b36284
SafitriPutri <117289241+SafitriPutri@users.noreply.github.com>
2024-06-02 18:58:40 +0700

    Update home.blade.php

--- home.blade.php
+++ home.blade.php
@@ -1,22 +1,14 @@
-<!DOCTYPE html>
-<html>
-<head>
-    <title>Personal Website</title>
-</head>
-<body>
-    <h1>Nama: John Doe</h1>
-    <img src="/path/to/photo.jpg" alt="Photo of John Doe">
-    <h2>Contact</h2>
-    <p>Email: safitriputri@student.ub.ac.id</p>
-    <p>Phone: 0822-4574-7512</p>
-    <h2>Background</h2>
-    <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Phasellus imperdiet...</p>
-    <h2>Skills</h2>
-    <ul>
-        <li>Web Development</li>
-        <li>PHP</li>
-        <li>Laravel</li>
-    </ul>
-    <a href="/projects">Projects</a>
-</body>
-</html>
+@extends('layouts.main')
+
+@section('Personal Website')
+
+@section('content')
+<h1>Nama: John Doe</h1>
+<img src="/path/to/photo.jpg" alt="Photo of John Doe">
+<h2>Contact</h2>
+<p>Email: safitriputri@student.ub.ac.id</p>
+<p>Phone: 0822-4574-7512</p>
+<h2>Background</h2>
+<p>Seorang mahasiswi yang sedang menempuh pendidikan S1 di jurusan Pendidikan Teknologi Informasi, Fakultas Ilmu Komputer, Universitas Brawijaya, Malang.</p>
+<a href="/projects">Projects</a>
+@endsection
